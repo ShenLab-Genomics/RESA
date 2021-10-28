dc5d32bb7ab5225864037445736abb8b32afdc7e
TZhang11 <63164949+TZhang11@users.noreply.github.com>
2021-10-28 08:16:49 +0100

    Add files via upload

---
+++ src/get_mutation_type_seq_context.R
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+###############################
+# Given a list of SNV vcf files
+# Extract the mutation type,
+# and 3bp sequence context
+# Written by Ning Shen
+# 2020-07-10
+###############################
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+if (!requireNamespace("MutationalPatterns", quietly = TRUE))
+  BiocManager::install("MutationalPatterns")
+if (!requireNamespace("BSgenome", quietly = TRUE))
+  BiocManager::install("BSgenome")
+if (!requireNamespace("BSgenome.Hsapiens.UCSC.hg38", quietly = TRUE))
+  BiocManager::install("BSgenome.Hsapiens.UCSC.hg38")
+if (!requireNamespace("stringr", quietly = TRUE))
+  install.packages("stringr")
+
+library(MutationalPatterns)
+library(BSgenome)
+library("BSgenome.Hsapiens.UCSC.hg38", character.only = TRUE)
+library(stringr)
+
+anno = function(vcf_file,sample_name,dir_out){
+  ref_genome = "BSgenome.Hsapiens.UCSC.hg38"
+  vcf_file <- c(vcf_file) 
+  vcfs = read_vcfs_as_granges(vcf_file, sample_name, ref_genome) 
+	type_context = type_context(vcfs[[1]],ref_genome)
+	out_matrix = data.frame("Variant"=names(vcfs[[1]]), "Mut_type"=type_context$types, "Seq_context"=type_context$context, "Mut_spec"= paste0(type_context$types, ".", type_context$context))
+	file_out = paste0(dir_out,sample_name[1])
+	write.table(out_matrix, file_out, sep="\t", quote=F, row.names=FALSE, col.names=TRUE)
+}
+
+
